c863ce58af071a3dee844dd8f1e94522ba0ce172
charlesoxendine <102759864+charlesoxendine@users.noreply.github.com>
2022-08-29 21:06:22 -0400

    updated example app

--- testSwiftyForms/ViewController.swift
+++ testSwiftyForms/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import EasyFormsPackage
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        showFormViewControllerTest()
+    }
+    
+    func showFormViewControllerTest() {
+        let usernameFormField = FormField(fieldTitle: "Username", fieldID: "0", fieldType: .textEntry)
+        let passwordFormField = FormField(fieldTitle: "Password", fieldID: "1", fieldType: .textEntry)
+        let rememberMeFormField = FormField(fieldTitle: "Remember Me", fieldID: "2", fieldType: .toggleButton)
+            
+        EasyFormsPackage.presentFormViewController(parentViewController: self, fields: [usernameFormField, passwordFormField, rememberMeFormField])
+    }
 }
 
+extension ViewController: FormViewControllerDelegate {
+    
+    func didSubmit(fields: [FormField], validateFieldEntries: (Bool, String?) -> ()) {
+        let usernameResponse = fields[0].textResponse
+        let passwordResponse = fields[1].textResponse
+        let rememberMeResponse = fields[2].toggleReponse
+        
+        guard usernameResponse != nil &&
+                passwordResponse != nil else {
+            validateFieldEntries(false, "Please fill in both fields.")
+            return
+        }
+        
+        if rememberMeResponse == true {
+            // Do something with this
+        }
+        
+        validateFieldEntries(true, nil)
+    }
+    
+    func didCancel() {
+        print("FORM CANCELED")
+    }
+
+}
